9833fb8e6bf583b347c1eec92bec28a22cc0385a
wariat85 <68301675+wariat85@users.noreply.github.com>
2025-11-11 00:56:17 +0100

    add program presets

--- custom_components/candy_bianca/const.py
+++ custom_components/candy_bianca/const.py
@@ -11,3 +11,13 @@ CONF_SCAN_INTERVAL = "scan_interval"
 
 DEFAULT_SCAN_INTERVAL = 30  # seconds
 DEFAULT_NAME = "Candy Bianca"
+
+PROGRAM_PRESETS = {
+    "Perfect Rapid 14 Min.": "PrNm=16&PrCode=7&PrStr=Rapido 14 Min.&SLevTgt=1&Dry=0",
+    "Perfect Rapid 30 Min.": "PrNm=16&PrCode=7&PrStr=Rapido 30 Min.&SLevTgt=2&Dry=0",
+    "Perfect Rapid 44 Min.": "PrNm=16&PrCode=7&PrStr=Rapido 44 Min.&SLevTgt=3&Dry=0",
+    "Perfect Rapid 59 Min.": "PrNm=15&PrCode=8&PrStr=Rapido 59 Min&SLevTgt=0&Dry=0",
+    "Asciugatura Misti (Extra Asciutto)": "PrNm=11&PrCode=77&PrStr=Asciuga Misti&SLevTgt=0&Dry=1",
+    "Asciugatura Misti (Pronto Stiro)": "PrNm=11&PrCode=77&PrStr=Asciuga Misti&SLevTgt=0&Dry=2",
+    "Asciugatura Misti (Pronto Armadio)": "PrNm=11&PrCode=77&PrStr=Asciuga Misti&SLevTgt=0&Dry=3"
+}
