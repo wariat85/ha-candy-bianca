0cda5e16ed745e609bb9ac7012ab737f7a3cd3c5
wariat85 <68301675+wariat85@users.noreply.github.com>
2025-11-19 01:17:23 +0100

    Add temperature and spin selects

--- custom_components/candy_bianca/const.py
+++ custom_components/candy_bianca/const.py
@@ -12,6 +12,9 @@ CONF_SCAN_INTERVAL = "scan_interval"
 DEFAULT_SCAN_INTERVAL = 30  # seconds
 DEFAULT_NAME = "Candy Bianca"
 
+TEMPERATURE_OPTIONS: list[int] = [0, 20, 30, 40, 60, 90]
+SPIN_OPTIONS: list[int] = list(range(0, 11))
+
 PROGRAM_PRESETS = {
     "Perfect Rapid 14 Min.": "PrNm=16&PrCode=7&PrStr=Rapido 14 Min.&SLevTgt=1&Dry=0",
     "Perfect Rapid 30 Min.": "PrNm=16&PrCode=7&PrStr=Rapido 30 Min.&SLevTgt=2&Dry=0",
